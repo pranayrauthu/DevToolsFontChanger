f9880ab80356da54c007439e46f553441a828cd9
Pranay Kumar <pranay.rauthu@gmail.com>
2017-12-09 17:35:53 +0530

    added option to modify font-family

--- devtools.js
+++ devtools.js
@@ -1,7 +1,17 @@
 
 
-fetch("/style.css").then(function (resp) {
-    return resp.text();
-}).then(function (respText) {
-    chrome.devtools.panels.applyStyleSheet(respText);
-})
+const styleTemplate = (fontFamily) => `
+
+    :host-context(.platform-windows) .monospace, 
+    :host-context(.platform-windows) .source-code, 
+    .platform-windows .monospace, .platform-windows .source-code{
+        font-family: ${fontFamily} !important;
+    }
+
+`;
+
+chrome.storage.sync.get({
+    fontFamily: 'monospace'
+}, function (items) {
+    chrome.devtools.panels.applyStyleSheet(styleTemplate(items.fontFamily));
+});
